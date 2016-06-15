87a0be80fe10ba9043955350c32c7f08b36e3af1
GarrisonSS <garrison@gyrosityprojects.co>
2016-06-15 16:11:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Crosshair Music? Just say HELLO to get started.')
+            return bot.say('Thanks for getting in touch! Just say HI to get started.')
                 .then(() => 'speak');
         }
     },
